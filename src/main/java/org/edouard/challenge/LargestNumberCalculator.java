758fad5f593565c745b344db7a6513bd56ee2a09
edou5rd <edou9rdc@gmail.com>
2022-07-29 17:06:33 +0100

    created new implementation and code cleaning.

--- src/main/java/org/edouard/challenge/LargestNumberCalculator.java
+++ src/main/java/org/edouard/challenge/LargestNumberCalculator.java
@@ -1,6 +1,5 @@
 package org.edouard.challenge;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -11,26 +10,39 @@ public final class LargestNumberCalculator {
     }
 
     public static String getLargestNumber(final List<Integer> numbersList) {
-        final List<String> numbersStringsList = convertToStringList(numbersList);
-        numbersStringsList.sort((x, y) -> {
-            final String xy = x + y;
-            final String yx = y + x;
-            return xy.compareTo(yx);
+        numbersList.sort(new Comparator<>() {
+            @Override
+            public int compare(final Integer x, final Integer y) {
+                final Double xWithMask = x * Math.pow(10, getLength(y));
+                final Double yWithMask = y * Math.pow(10, getLength(x));
+                final Double xy = xWithMask + y;
+                final Double yx = yWithMask + x;
+                return xy.compareTo(yx);
+            }
+
+            private int getLength(int number) {
+                int length;
+
+                if (number == 0)
+                    length = 1;
+                else
+                    length = 0;
+
+                while (number > 0) {
+                    length++;
+                    number /= 10;
+                }
+                return length;
+            }
+
         });
-        Collections.reverse(numbersStringsList);
-        return LargestNumberListToString(numbersStringsList);
+        Collections.reverse(numbersList);
+        return LargestNumberListToString(numbersList);
     }
 
-    private static List<String> convertToStringList(final List<Integer> numbersList) {
-        final List<String> list = new ArrayList<>();
-        for (final Integer i : numbersList){
-            list.add(i.toString());
-        }
-        return list;
-    }
-    private static String LargestNumberListToString(final List<String> numbersList) {
+    private static String LargestNumberListToString(final List<Integer> numbersList) {
         final var stringBuilder = new StringBuilder();
-        for (final String n : numbersList){
+        for (final int n : numbersList){
             stringBuilder.append(n);
         }
         return stringBuilder.toString();
--- src/test/java/org/edouard/challenge/LargestNumberCalculatorTest.java
+++ src/test/java/org/edouard/challenge/LargestNumberCalculatorTest.java
@@ -3,38 +3,56 @@ package org.edouard.challenge;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 class LargestNumberCalculatorTest {
 
     private static Stream<Arguments> shouldReturnExpectedOutputMethodSource() {
+        final List<Integer> list1 = new  ArrayList<>();
+        final List<Integer> list2 = Arrays.asList(5);
+        final List<Integer> list3 = Arrays.asList(12, 5, 0);
+        final List<Integer> list4 = Arrays.asList(1,2,3);
+        final List<Integer> list5 = Arrays.asList(9,93);
+        final List<Integer> list6 = Arrays.asList(50,2,1,9);
+        final List<Integer> list7 = Arrays.asList(5,50,56);
+        final List<Integer> list8 = Arrays.asList(420,42,423);
+        final List<Integer> list9 = Arrays.asList(839,83);
+        final List<Integer> list10 = Arrays.asList(Integer.MAX_VALUE,83);
+        final List<Integer> list11 = Arrays.asList(Integer.MAX_VALUE, 99, Integer.MAX_VALUE, Integer.MAX_VALUE, 12);
+        final List<Integer> list12 = Arrays.asList(Integer.MAX_VALUE, (Integer.MAX_VALUE -1), 99);
+        final List<Integer> list13 = Arrays.asList(0, 99, 0);
+
         return Stream.of(
-                arguments("empty list", List.of(), ""),
-                arguments("list with one number", List.of(5), "5"),
-                arguments("list of 3 random numbers with a 0", List.of(12, 5, 0), "5120"),
-                arguments("list of 3 random numbers", List.of(1,2,3), "321"),
-                arguments("list of 2 random numbers", List.of(9,93), "993"),
-                arguments("list of 4 random numbers", List.of(50,2,1,9), "95021"),
-                arguments("example 1", List.of(5,50,56), "56550"),
-                arguments("example 2", List.of(420,42,423), "42423420"),
-                arguments("example 3", List.of(839,83), "83983"),
-                arguments("List with max Integer", List.of(Integer.MAX_VALUE,83), "83" + Integer.MAX_VALUE),
-                arguments("List with multiple values max Integer", List.of(Integer.MAX_VALUE, 99, Integer.MAX_VALUE,
-                        Integer.MAX_VALUE, 12), "99" + Integer.MAX_VALUE  + Integer.MAX_VALUE + Integer.MAX_VALUE
-                        + "12")
+                arguments("empty list", list1, ""),
+                arguments("list with one number", list2, "5"),
+                arguments("list of 3 random numbers with a 0", list3, "5120"),
+                arguments("list of 3 random numbers", list4, "321"),
+                arguments("list of 2 random numbers", list5, "993"),
+                arguments("list of 4 random numbers", list6, "95021"),
+                arguments("example 1", list7, "56550"),
+                arguments("example 2", list8, "42423420"),
+                arguments("example 3", list9, "83983"),
+                arguments("List with max Integer", list10, "83" + Integer.MAX_VALUE),
+                arguments("List with multiple values max Integer", list11,
+                        "99" + Integer.MAX_VALUE  + Integer.MAX_VALUE + Integer.MAX_VALUE + "12"),
+                arguments("test precision.", list12, "99" + Integer.MAX_VALUE  + (Integer.MAX_VALUE -1)),
+                arguments("multiple null values .", list13, "9900")
                 );
     }
 
     @MethodSource("shouldReturnExpectedOutputMethodSource")
     @ParameterizedTest(name = "should_returnExpectedOutput - {0}")
-    void should_returnExpectedOutput (final String testMessage, final List<Integer> numbers, final String expectedOutput){
-        assertEquals(expectedOutput, LargestNumberCalculator.getLargestNumber(numbers));
+    void should_returnExpectedOutput (final String testMessage,
+                                      List<Integer> numbers,
+                                      final String expectedOutput){
+        assertEquals(expectedOutput, LargestNumberCalculator.getLargestNumber(numbers), testMessage);
     }
 
 }
